cefdf9b5ab5b1cedd1a4dd9a851126607f4f95f9
dawe <dawedawe@posteo.de>
2023-12-01 23:45:45 +0100

    Day 1 Part 2

--- Day01.ex
+++ Day01.ex
@@ -30,7 +30,68 @@ defmodule Day01 do
     |> parseInput()
     |> Enum.sum()
   end
+
+  def replace(s, acc, prefix, replacementStr, replacementNr, continuation) do
+    s2 = String.replace_prefix(s, prefix, replacementStr)
+    acc2 = acc <> replacementNr
+    continuation.(s2, acc2)
+  end
+
+  def foldFunc(s, acc) do
+    cond do
+      String.starts_with?(s, "one") ->
+        replace(s, acc, "one", "e", "1", &foldFunc/2)
+
+      String.starts_with?(s, "two") ->
+        replace(s, acc, "two", "o", "2", &foldFunc/2)
+
+      String.starts_with?(s, "three") ->
+        replace(s, acc, "three", "e", "3", &foldFunc/2)
+
+      String.starts_with?(s, "four") ->
+        replace(s, acc, "four", "", "4", &foldFunc/2)
+
+      String.starts_with?(s, "five") ->
+        replace(s, acc, "five", "e", "5", &foldFunc/2)
+
+      String.starts_with?(s, "six") ->
+        replace(s, acc, "six", "", "6", &foldFunc/2)
+
+      String.starts_with?(s, "seven") ->
+        replace(s, acc, "seven", "n", "7", &foldFunc/2)
+
+      String.starts_with?(s, "eight") ->
+        replace(s, acc, "eight", "t", "8", &foldFunc/2)
+
+      String.starts_with?(s, "nine") ->
+        replace(s, acc, "nine", "e", "9", &foldFunc/2)
+
+      s == "" ->
+        acc
+
+      true ->
+        {s1, s2} = String.split_at(s, 1)
+        acc2 = acc <> s1
+        foldFunc(s2, acc2)
+    end
+  end
+
+  def parseInput2(s) do
+    s
+    |> String.split(["\n", "\r\n"], trim: true)
+    |> Enum.map(&foldFunc(&1, ""))
+    |> Enum.map(&parseLine(&1))
+  end
+
+  def day01Part2 do
+    readInput()
+    |> parseInput2()
+    |> Enum.sum()
+  end
 end
 
 Day01.day01Part1()
+|> IO.puts()
+
+Day01.day01Part2()
 |> IO.puts()
