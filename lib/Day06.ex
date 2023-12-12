e1b98ad759db287c9f766a3b5603238ce7a0f410
dawe <dawedawe@posteo.de>
2023-12-12 23:06:03 +0100

    Day 6 Part 2

--- lib/Day06.ex
+++ lib/Day06.ex
@@ -27,7 +27,27 @@ defmodule Day06 do
     |> Enum.map(&calc/1)
     |> Enum.product()
   end
+
+  def parse_input2(s) do
+    s
+    |> String.split(["\n", "\r\n"], trim: false)
+    |> Enum.map(fn line ->
+      Regex.scan(~r/\d+/, line)
+      |> Enum.concat()
+      |> Enum.join()
+      |> then(fn s -> Integer.parse(s) |> elem(0) end)
+    end)
+  end
+
+  def part2() do
+    File.read!(input_file())
+    |> parse_input2()
+    |> then(fn [t, d] -> calc({t, d}) end)
+  end
 end
 
 Day06.part1()
+|> IO.inspect()
+
+Day06.part2()
 |> IO.inspect()
