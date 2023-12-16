cde5f13ce91e9f4559f16c9694aff7030febf1d9
dawe <dawedawe@posteo.de>
2023-12-16 23:30:22 +0100

    Day 7 Part 2

--- lib/Day07.ex
+++ lib/Day07.ex
@@ -80,19 +80,160 @@ defmodule Day07 do
     end
   end
 
-  def type_strength(c) do
-    m =
-      %{
-        :five_of_a_kind => 6,
-        :four_of_a_kind => 5,
-        :full_house => 4,
-        :three_of_a_kind => 3,
-        :two_pair => 2,
-        :one_pair => 1,
-        :is_high_card => 0
-      }
+  def type_strength_map() do
+    %{
+      :five_of_a_kind => 6,
+      :four_of_a_kind => 5,
+      :full_house => 4,
+      :three_of_a_kind => 3,
+      :two_pair => 2,
+      :one_pair => 1,
+      :is_high_card => 0
+    }
+  end
 
-    m[type_of_card(c)]
+  def type_strength(c) do
+    t = type_of_card(c)
+    type_strength_map()[t]
+  end
+
+  def is_five_of_a_kind_possible?(cards) do
+    is_five_of_a_kind?(cards) ||
+      cards
+      |> Enum.filter(&(&1 != "J"))
+      |> Enum.group_by(& &1)
+      |> Map.values()
+      |> then(&(length(&1) == 1))
+  end
+
+  def is_four_of_a_kind_possible?(cards) do
+    is_four_of_a_kind?(cards) ||
+      (
+        j_count = cards |> Enum.count(&(&1 == "J"))
+
+        cards
+        |> Enum.filter(&(&1 != "J"))
+        |> Enum.group_by(& &1)
+        |> Map.values()
+        |> Enum.max_by(&length/1)
+        |> then(&(length(&1) + j_count == 4))
+      )
+  end
+
+  def is_full_house_possible?(cards) do
+    is_full_house?(cards) ||
+      (
+        j_count = cards |> Enum.count(&(&1 == "J"))
+
+        groups =
+          cards
+          |> Enum.filter(&(&1 != "J"))
+          |> Enum.group_by(& &1)
+          |> Map.values()
+
+        cond do
+          j_count == 1 -> length(groups) == 2
+          j_count == 2 -> length(groups) == 2
+          j_count == 3 -> length(groups) >= 1
+          true -> raise("bad state")
+        end
+      )
+  end
+
+  def is_three_of_a_kind_possible?(cards) do
+    is_three_of_a_kind?(cards) ||
+      (
+        j_count = cards |> Enum.count(&(&1 == "J"))
+
+        groups =
+          cards
+          |> Enum.filter(&(&1 != "J"))
+          |> Enum.group_by(& &1)
+          |> Map.values()
+
+        cond do
+          j_count == 1 -> length(groups) == 2 || length(groups) == 3
+          j_count == 2 -> length(groups) <= 3
+          j_count == 3 -> length(groups) <= 2
+          true -> raise("bad state")
+        end
+      )
+  end
+
+  def is_two_pair_possible?(cards) do
+    is_two_pair?(cards) ||
+      (
+        j_count = cards |> Enum.count(&(&1 == "J"))
+
+        groups =
+          cards
+          |> Enum.filter(&(&1 != "J"))
+          |> Enum.group_by(& &1)
+          |> Map.values()
+
+        cond do
+          j_count == 1 -> length(groups) == 2
+          j_count == 2 -> true
+          true -> raise("bad state")
+        end
+      )
+  end
+
+  def is_one_pair_possible?(cards) do
+    is_one_pair?(cards) ||
+      (
+        j_count = cards |> Enum.count(&(&1 == "J"))
+
+        groups =
+          cards
+          |> Enum.filter(&(&1 != "J"))
+          |> Enum.group_by(& &1)
+          |> Map.values()
+
+        cond do
+          j_count == 1 -> length(groups) == 4 || length(groups) == 3
+          j_count == 2 -> length(groups) == 3 || length(groups) == 2
+          true -> raise("bad state")
+        end
+      )
+  end
+
+  def try_joker(c, t) do
+    cond do
+      is_five_of_a_kind_possible?(c) ->
+        :five_of_a_kind
+
+      is_four_of_a_kind_possible?(c) ->
+        :four_of_a_kind
+
+      is_full_house_possible?(c) ->
+        :full_house
+
+      is_three_of_a_kind_possible?(c) ->
+        :three_of_a_kind
+
+      is_two_pair_possible?(c) ->
+        :two_pair
+
+        is_one_pair_possible?(c) ->
+        :one_pair
+
+      true ->
+        t
+    end
+  end
+
+  def type_strength_part2(c) do
+    t = type_of_card(c)
+
+    t2 =
+      if Enum.member?(c, "J") do
+        try_joker(c, t)
+      else
+        t
+      end
+
+    type_strength_map()[t2]
   end
 
   def card_strength(c) do
@@ -116,12 +257,33 @@ defmodule Day07 do
     m[c]
   end
 
-  def compare_relative(a, b) do
+  def card_strength_part2(c) do
+    m =
+      %{
+        "A" => 12,
+        "K" => 11,
+        "Q" => 10,
+        "T" => 9,
+        "9" => 8,
+        "8" => 7,
+        "7" => 6,
+        "6" => 5,
+        "5" => 4,
+        "4" => 3,
+        "3" => 2,
+        "2" => 1,
+        "J" => 0
+      }
+
+    m[c]
+  end
+
+  def compare_relative(a, b, f) do
     {x, y} =
       Enum.zip(a, b)
       |> Enum.find(fn {x, y} -> x != y end)
 
-    if card_strength(x) < card_strength(y) do
+    if f.(x) < f.(y) do
       true
     else
       false
@@ -137,7 +299,7 @@ defmodule Day07 do
     cond do
       a_type_strength < b_type_strength -> true
       a_type_strength > b_type_strength -> false
-      a_type_strength == b_type_strength -> compare_relative(a, b)
+      a_type_strength == b_type_strength -> compare_relative(a, b, &card_strength/1)
     end
   end
 
@@ -148,7 +310,31 @@ defmodule Day07 do
     |> Enum.map(fn {{_, bid}, rank} -> bid * rank end)
     |> Enum.sum()
   end
+
+  def compare_part2({card_a, _bid_a}, {card_b, _bid_b}) do
+    a = String.graphemes(card_a)
+    b = String.graphemes(card_b)
+    a_type_strength = type_strength_part2(a)
+    b_type_strength = type_strength_part2(b)
+
+    cond do
+      a_type_strength < b_type_strength -> true
+      a_type_strength > b_type_strength -> false
+      a_type_strength == b_type_strength -> compare_relative(a, b, &card_strength_part2/1)
+    end
+  end
+
+  def part2() do
+    parse()
+    |> Enum.sort(&compare_part2/2)
+    |> Enum.with_index(1)
+    |> Enum.map(fn {{_, bid}, rank} -> bid * rank end)
+    |> Enum.sum()
+  end
 end
 
 Day07.part1()
+|> IO.inspect()
+
+Day07.part2()
 |> IO.inspect()
