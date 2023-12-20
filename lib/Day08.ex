34f33bb428f3c26e9d473bd345556169158333f4
dawe <dawedawe@posteo.de>
2023-12-20 23:10:54 +0100

    Day 8 Part 2

--- lib/Day08.ex
+++ lib/Day08.ex
@@ -23,12 +23,12 @@ defmodule Day08 do
     end)
   end
 
-  def count_steps(instructions, map, inst_ptr, current_node, acc)
+  def count_steps(instructions, map, inst_ptr, current_node, acc, end_predicate)
       when inst_ptr == length(instructions) do
-    count_steps(instructions, map, 0, current_node, acc)
+    count_steps(instructions, map, 0, current_node, acc, end_predicate)
   end
 
-  def count_steps(instructions, map, inst_ptr, current_node, acc) do
+  def count_steps(instructions, map, inst_ptr, current_node, acc, end_predicate) do
     h = instructions |> Enum.at(inst_ptr)
     next_acc = acc + 1
 
@@ -39,19 +39,51 @@ defmodule Day08 do
         true -> raise("unsupported instructions #{h}")
       end
 
-    if next_node == "ZZZ" do
+    if end_predicate.(next_node) do
       next_acc
     else
-      count_steps(instructions, map, inst_ptr + 1, next_node, next_acc)
+      count_steps(instructions, map, inst_ptr + 1, next_node, next_acc, end_predicate)
     end
   end
 
   def part1() do
     {instructions, map} = parse()
+    count_steps(instructions, map, 0, "AAA", 0, fn n -> n == "ZZZ" end)
+  end
 
-    count_steps(instructions, map, 0, "AAA", 0)
+  def count_steps2(current, max, steps) do
+    all_good = Enum.all?(steps, fn x -> rem(current, x) == 0 end)
+
+    if all_good do
+      current
+    else
+      next_current = current + max
+      count_steps2(next_current, max, steps)
+    end
+  end
+
+  def part2() do
+    {instructions, map} = parse()
+
+    current_nodes =
+      map
+      |> Map.keys()
+      |> Enum.filter(fn s -> String.ends_with?(s, "A") end)
+
+    steps =
+      current_nodes
+      |> Enum.map(fn n ->
+        count_steps(instructions, map, 0, n, 0, fn n -> String.ends_with?(n, "Z") end)
+      end)
+
+    max = Enum.max(steps)
+    steps_wo_max = Enum.filter(steps, &(&1 != max))
+    count_steps2(max, max, steps_wo_max)
   end
 end
 
 Day08.part1()
+|> IO.inspect()
+
+Day08.part2()
 |> IO.inspect()
